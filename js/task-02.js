af7e9bcf86a311ac12e230b45225e9d2a8dd2cca
vasylenkodi <vasylenko1daniil@gmail.com>
2023-01-26 15:17:32 +0100

    добавляет решение второй задачи

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listElement = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) { //перебирает массив ингредиентов
+  const listItem = document.createElement('li');  //создает элемент ли
+  listItem.textContent = ingredient; //записывает ингредиент в элемент ли
+  listItem.classList.add('item'); //добавляет класс итем
+  listElement.append(listItem); //добавляет ли в юл
+}
