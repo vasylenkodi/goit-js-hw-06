bef06f2640cb62c52248746aecbc722ff550bcf0
vasylenkodi <vasylenko1daniil@gmail.com>
2023-01-27 15:24:16 +0100

    добавляет решение задания 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyElement = document.querySelector("body");
+const btnElement = document.querySelector(".change-color");
+const textElement = document.querySelector(".color");
+
+const onBodyBgcChange = () => {
+  const bodyBgc = getRandomHexColor();
+
+  bodyElement.style.backgroundColor = bodyBgc;
+  textElement.textContent = bodyBgc;
+};
+
+btnElement.addEventListener('click', onBodyBgcChange);
