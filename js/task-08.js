7fc1692bfa3b6c762cf10e9d2a77c7164ceabab0
vasylenkodi <vasylenko1daniil@gmail.com>
2023-01-27 15:17:48 +0100

    добавляет решение задания 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formElement = document.querySelector(".login-form");
 
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    if (formElements.email.value === "" || formElements.password.value === '') {
+        window.alert('Все поля должны быть заполнены')
+    } else {
+        const formData = new FormData(event.currentTarget);
+        formData.forEach((value, name) => {
+            formData[name] = value;
+        })
+        console.log(formData);
+        event.currentTarget.reset();
+    }
+});
